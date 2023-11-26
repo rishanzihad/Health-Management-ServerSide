ef1a3ed817961f2773f53e680b5fccda31065c26
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-11-26 23:15:03 +0600

    camps Post done

--- index.js
+++ index.js
@@ -27,12 +27,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    app.post('/camps', async (req, res) => {
+      const camps = req.body;
+      const result = await campCollection.insertOne(camps);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
