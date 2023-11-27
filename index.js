d88c1a8f4b9737add40494895bbdad39d853fa67
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-11-27 10:37:10 +0600

    get user

--- index.js
+++ index.js
@@ -67,6 +67,21 @@ async function run() {
     //await client.close();
   }
 }
+app.get('/users/admin/:email', async (req, res) => {
+  const email = req.params.email;
+
+  if (email !== req.decoded.email) {
+    return res.status(403).send({ message: 'forbidden access' })
+  }
+
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  let admin = false;
+  if (user) {
+    admin = user?.role === 'admin';
+  }
+  res.send({ admin });
+})
 run().catch(console.dir);
 
 
